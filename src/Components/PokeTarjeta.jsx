b9942f34f0ba887636db6d6cba7b190ccdbfae2e
Misael Reynoso <misael.reynoso.aguayo@gmail.com>
2024-09-03 19:52:59 -0600

    feat(index.jsx): añadir nuevas funciones para los pokemones - Mostrar Pokemos - Filtro de busqueda

--- src/Components/PokeTarjeta.jsx
+++ src/Components/PokeTarjeta.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Col, Card, CardBody, CardFooter, CardImg, Badge, Label } from "reactstrap"
+import { Link } from "react-router-dom";
 
-const PokeTarjeta = () => {
+
+const PokeTarjeta = (params) => {
+  const [pokemon,setPokemon] = useState([]);
+  const [imagen,setImagen] = useState('');
+  const [cardClass,setCardClass] = useState('d-none');
+  const [loadClass,setLoadClass] = useState('');
+  useEffect(() =>{
+    getPokemon()
+  },[])
+  const getPokemon = async() =>{
+    const liga = params.poke.url;
+    axios.get(liga).then( async(response)=>{
+      const respuesta = response.data;
+      setPokemon(respuesta);
+      if (respuesta.sprites.other.dream_world.front_default != null) {
+        setImagen(respuesta.sprites.other.dream_world.front_default)
+      }else{
+        setImagen(respuesta.sprites.other['official-artwork'].front_default);
+
+      }
+      setCardClass('');
+      setLoadClass('d-none')
+    })
+  }
   return (
-    <div>PokeTarjeta</div>
+    <Col sm="4" lg="3" className="mb-3">
+      <Card className={"shadow border-4 border-warning "+loadClass}>
+        <CardImg src="/img/loading1.webp" height="200" className="p-3"></CardImg>
+      </Card>
+      <Card className={"shadow border-4 border-warning "+ cardClass}>
+        <CardImg src={imagen} height="150" className="p-2" />
+        <CardBody className="text-center">
+          <Badge pill color="danger"># {pokemon.id}</Badge>
+          <Label className="fs-4 text-capitalize"> {pokemon.name}</Label>
+        </CardBody>
+        <CardFooter className="bg-warning">
+          <Link className="btn btn-dark">
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>  Detalle
+          </Link>
+        </CardFooter>
+      </Card>
+    </Col>
   )
 }
 
--- src/Views/Index.jsx
+++ src/Views/Index.jsx
@@ -1,14 +1,18 @@
 import { Container, Row, Col, InputGroup, InputGroupText, Input } from "reactstrap"
-import axios from "axios"
+import axios, { all } from "axios"
 import { useState, useEffect } from "react"
 import PokeTarjeta from "../Components/PokeTarjeta";
 
 const Index = () => {
   const [pokemones,setPokemones] = useState([]);
+  const [allpokemones,setAllPokemones] = useState([]);
+  const [listado,setListado] = useState([]);
+  const [filtro,setFiltro] = useState('');
   const [offset,setOffset] = useState(0);
   const [limit,setLimit] = useState(20);
   useEffect(() =>{
     getPokemones(offset)
+    getAllPokemones()
   },[])
 
 
@@ -17,8 +21,32 @@ const Index = () => {
     axios.get(liga).then( async (response) => {
       const respuesta = response.data;
       setPokemones(respuesta.results)
-      console.log(pokemones)
+      setListado(respuesta.results)
     })
+  }
+
+  const getAllPokemones = async(o) =>{
+    const liga = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+    axios.get(liga).then( async (response) => {
+      const respuesta = response.data;
+      setAllPokemones(respuesta.results)
+    })
+  }
+
+  const buscar = async(e) =>{
+    if (e.keyCode == 13) {
+      if(filtro.trim() != ''){
+        setListado([]);
+        setTimeout(()=>{
+          setListado(allpokemones.filter(p => p.name.includes(filtro)))
+        },100)
+      }
+    } else if(filtro.trim() == ''){
+      setListado([]);
+      setTimeout(()=>{
+        setListado(pokemones)
+      },100)
+    }
   }
 
   return (
@@ -27,12 +55,13 @@ const Index = () => {
         <Col>
           <InputGroup className="mt-3 mb-3">
           <InputGroupText><i className="fa-solid fa-search"></i></InputGroupText>
-          <Input placeholder="Buscar pokemon"></Input>
+          <Input value={filtro} onChange={(e)=>{setFiltro(e.target.value)}} 
+            onKeyUpCapture={buscar} placeholder="Buscar pokemon"></Input>
           </InputGroup>
         </Col>
       </Row>
       <Row className="mt-3">
-        {pokemones.map((pok,i)=>(
+        {listado.map((pok,i)=>(
           <PokeTarjeta poke={pok} key={i}/>
         ))}
 
